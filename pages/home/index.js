cf6eea5be68600a6c8f117bd620fadda365c64c2
Nicolás Molina <nmolina@teco.com.ar>
2021-07-14 20:35:01 -0300

    Add github session working ok

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,21 @@
+import { signOut, useSession } from "next-auth/client"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 export default function Home() {
+  const [session, loading] = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!session && !loading) router.push("/")
+  }, [session, loading])
+
+  if (typeof window !== "undefined" && loading) return null
+
   return (
     <>
       <h1>HOLA PAGINA HOME!!!</h1>
+      <div>{<button onClick={signOut}>LogOut GitHub</button>}</div>
     </>
   )
 }
