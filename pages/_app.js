40a55d9bdb7f681c2444c27effb8288253fc6c17
Nicolás Molina <nmolina@teco.com.ar>
2021-08-24 14:31:21 -0300

    Delete chakra ui, fix some folder wihtout components

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import "../styles/globals.css"
 import { Provider } from "next-auth/client"
-import { ChakraProvider } from "@chakra-ui/react"
 import AuthGuard from "../helpers/hocs/auth"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Provider session={pageProps.session}>
-        {Component.requireAuth ? (
-          <AuthGuard>
-            <Component {...pageProps} />
-          </AuthGuard>
-        ) : (
+    <Provider session={pageProps.session}>
+      {Component.requireAuth ? (
+        <AuthGuard>
           <Component {...pageProps} />
-        )}
-      </Provider>
-    </ChakraProvider>
+        </AuthGuard>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </Provider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,20 @@
 import { useEffect } from "react"
 import { useRouter } from "next/router"
-import { useSession, signIn } from "next-auth/client"
-import { Button, Flex, Text, useColorModeValue } from "@chakra-ui/react"
-import styles from "../styles/Home.module.css"
+import { useSession } from "next-auth/client"
+// import styles from "../styles/Home.module.css"
 
 export default function Home() {
   const [session, loading] = useSession()
   const router = useRouter()
-  const formBackground = useColorModeValue("gray.100", "gray.400")
 
   useEffect(() => {
     if (session && !loading) router.replace("/home")
   }, [session, loading])
 
-  return (
-    <div>
-      {!session && !loading && (
-        <Flex className={styles.container}>
-          <Flex
-            className={styles.loginContainer}
-            direction="column"
-            background={formBackground}
-            p={12}
-            rounded={12}
-          >
-            <Text fontSize="3xl" mb={10}>
-              Log In Options
-            </Text>
-            <Button
-              colorScheme="teal"
-              onClick={(e) => {
-                e.preventDefault()
-                signIn(null, {
-                  callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/home`,
-                })
-              }}
-            >
-              Log in with GitHub
-            </Button>
-          </Flex>
-        </Flex>
-      )}
-    </div>
-  )
+  // Repo challenge a implementar
+  // https://github.com/goncy/basement-challenge
+  // Figma
+  // https://www.figma.com/file/BYjaSbdPyhEL0ucneDlIQ0/Dev-Challenge?node-id=1%3A218
+
+  return <div></div>
 }
