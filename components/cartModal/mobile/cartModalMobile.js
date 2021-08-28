6fb70f8dae99a1530f96b02fc6c13f0cbe284d22
Nicolas Molina <nicomoli91@gmail.com>
2021-08-28 20:10:59 -0300

    Add some styles and icons to cart modal mobile

--- components/cartModal/mobile/cartModalMobile.js
+++ components/cartModal/mobile/cartModalMobile.js
@@ -17,11 +17,27 @@ const CartModalMobile = ({ close }) => {
           <Image src={"/your-cart.svg"} width={344} height={214}></Image>
         </div>
         <div className={styles.cartItem}>
-          <Image
-            src={"/remera-basement-black.png"}
-            width={98}
-            height={97}
-          ></Image>
+          <div className={styles.itemImage}>
+            <Image
+              src={"/remera-basement-black.png"}
+              width={98}
+              height={97}
+            ></Image>
+          </div>
+          <div className={styles.cartItemDescription}>
+            <p className={[styles.text, styles.descriptionItem].join(' ')}>Black T-Shirt</p>
+            <p className={[styles.text, styles.descriptionSubItem].join(' ')}>Unisex black soft style</p>
+            <p className={styles.text}>QUANTITY: 3</p>
+            <p className={styles.text}>Size M</p>
+            <p className={styles.text}>$ 12</p>
+          </div>
+        </div>
+        <div className={styles.totalPrice}>
+          <p>TOTAL</p>
+          <p>$ 37.50</p>
+        </div>
+        <div className={styles.footerImage}>
+          <Image src={'/checkout.svg'} width={343} height={62}></Image>
         </div>
       </div>
     </>
