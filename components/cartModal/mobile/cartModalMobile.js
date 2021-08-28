90a13d6d4352e928523b214bc9e9dff0becb4ed7
Nicolás Molina <nmolina@teco.com.ar>
2021-08-27 22:47:53 -0300

    Add some styles to cart mobile

--- components/cartModal/mobile/cartModalMobile.js
+++ components/cartModal/mobile/cartModalMobile.js
@@ -5,14 +5,24 @@ const CartModalMobile = ({ close }) => {
   return (
     <>
       <div className={styles.container}>
-        <Image
-          className={styles.closeIcon}
-          onClick={() => close()}
-          src={"/closeIcon.svg"}
-          width={80}
-          height={20}
-        ></Image>
-        <Image src={"/your-cart.svg"} width={344} height={214}></Image>
+        <div className={styles.closeIcon}>
+          <Image
+            onClick={() => close()}
+            src={"/closeIcon.svg"}
+            width={80}
+            height={20}
+          ></Image>
+        </div>
+        <div className={styles.yourCart}>
+          <Image src={"/your-cart.svg"} width={344} height={214}></Image>
+        </div>
+        <div className={styles.cartItem}>
+          <Image
+            src={"/remera-basement-black.png"}
+            width={98}
+            height={97}
+          ></Image>
+        </div>
       </div>
     </>
   )
