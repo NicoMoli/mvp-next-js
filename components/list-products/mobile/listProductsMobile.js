e674ad389f1ef1bb3fd1381a6628d88a6a4348da
Nicolas Molina <nicomoli91@gmail.com>
2021-08-29 20:53:05 -0300

    Create logic to save products in local storage

--- components/list-products/mobile/listProductsMobile.js
+++ components/list-products/mobile/listProductsMobile.js
@@ -2,6 +2,15 @@ import styles from "./listProductsMobile.module.css"
 import Image from "next/image"
 
 const ListProductsMobile = ({ products }) => {
+  const addItem = (item) => {
+    if (typeof window !== "undefined") {
+      const itemsSaveLocally =
+        JSON.parse(window.localStorage.getItem("cart")) || []
+      itemsSaveLocally.push(item)
+      window.localStorage.setItem("cart", JSON.stringify(itemsSaveLocally))
+    }
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -16,7 +25,7 @@ const ListProductsMobile = ({ products }) => {
 
         <div className={styles.itemsContainer}>
           {products?.map((item, key) => (
-            <div key={key}>
+            <div key={key} onClick={() => addItem(item)}>
               <div className={styles.itemImage}>
                 <Image src={item.image} width={361} height={360}></Image>
               </div>
