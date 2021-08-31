066ec4987c218f3557358f2064169e63ea415615
Nicolás Molina <nmolina@teco.com.ar>
2021-08-31 17:47:22 -0300

    Header desktop and icons

--- components/header/desktop/headerDesktop.js
+++ components/header/desktop/headerDesktop.js
@@ -1,9 +1,22 @@
 import styles from "./headerDesktop.module.css"
+import Image from "next/image"
 
-const HeaderDesktop = () => {
+const HeaderDesktop = ({ totalItems, toggle }) => {
   return (
     <>
-      <h3 className={styles.container}>HEADER DESKTOP</h3>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image src={"/basement-logo.svg"} width={192} height={28}></Image>
+        </div>
+        <div className={styles.groupIcons}>
+          <Image src={"/icons-group.svg"} width={284} height={25}></Image>
+        </div>
+        <div>
+          <button onClick={() => toggle()} className={styles.buttonCart}>
+            {totalItems ? `CART (${totalItems}) ` : "CART (0)"}
+          </button>
+        </div>
+      </div>
     </>
   )
 }
