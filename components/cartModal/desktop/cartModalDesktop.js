b56165389a51dd2b08ed434d8e3e515b602f1464
Nicolás Molina <nmolina@teco.com.ar>
2021-09-03 17:18:12 -0300

    Start build cart modal for desktop, rest details

--- components/cartModal/desktop/cartModalDesktop.js
+++ components/cartModal/desktop/cartModalDesktop.js
@@ -1,9 +1,64 @@
 import styles from "./cartModalDesktop.module.css"
+import Image from "next/image"
+import { useEffect, useState } from "react"
 
-const CartModalDesktop = () => {
+const CartModalDesktop = ({ itemsOnCart, close }) => {
+  const [items, setItems] = useState(null)
+
+  // TODO: MEJORAR ESTO!!!
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const itemsStorage = JSON.parse(window.localStorage.getItem("cart"))
+      setItems(itemsStorage)
+    }
+  }, [])
+
   return (
     <>
-      <div className={styles.container}>MODAL DESKTOP!!!!</div>
+      <div className={styles.container}>
+        <div className={styles.closeIcon}>
+          <Image
+            onClick={() => close()}
+            src={"/closeIcon.svg"}
+            width={80}
+            height={20}
+          ></Image>
+        </div>
+        <div className={styles.yourCart}>
+          <Image
+            src={"/your-cart-desktop.svg"}
+            width={344}
+            height={214}
+          ></Image>
+        </div>
+
+        {items?.map((item, key) => (
+          <div key={key} className={styles.cartItem}>
+            <div className={styles.itemImage}>
+              <Image src={item.image} width={98} height={97}></Image>
+            </div>
+            <div className={styles.cartItemDescription}>
+              <p className={[styles.text, styles.descriptionItem].join(" ")}>
+                {item.name}
+              </p>
+              <p className={[styles.text, styles.descriptionSubItem].join(" ")}>
+                {item.description}
+              </p>
+              <p className={styles.text}>QUANTITY: 3</p>
+              <p className={styles.text}>Size M</p>
+              <p className={styles.text}>$ {item.price}</p>
+            </div>
+          </div>
+        ))}
+
+        <div className={styles.totalPrice}>
+          <p>TOTAL</p>
+          <p>$ 37.50</p>
+        </div>
+        <div className={styles.footerImage}>
+          <Image src={"/checkout.svg"} width={343} height={62}></Image>
+        </div>
+      </div>
     </>
   )
 }
